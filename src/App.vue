9a65adcd1229278acf9c435e09533017b349cf93
hansol <onesorry49@gmail.com>
2018-12-21 22:06:00 +0900

    learn make todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import header from './components/Header.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'app-header': header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import cywhan from './cywhan/Cywhan.vue'
+import hansol from './hansol/Hansol.vue'
+import hyeonu from './hyeonu/Hyeonu.vue'
+
+export default [
+  { path: '/cywhan', component: cywhan },
+  { path: '/hansol', component: hansol },
+  { path: '/hyeonu', component: hyeonu }
+]
